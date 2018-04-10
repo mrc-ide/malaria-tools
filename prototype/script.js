bb0185c502af288f73a90e3b3c8f326b43d12278
Alex Hill <alex.hill@gmail.com>
2018-04-10 14:12:27 +0100

    add jquery ui slider

--- prototype/script.js
+++ prototype/script.js
@@ -1,28 +1,29 @@
 $(function () {
-    $(".scenario-bar a").click(function() {
-        $(".content-pane").hide();
+    $(".scenario-bar a").click(function () {
+        $(".content-pane").removeClass("in");
         $(".section-bar").find(".active").removeClass("active");
         $(".section-bar ul").find("a").first().addClass("active");
-        $(".interventions.content-pane").show();
-    });
-    $(".interventions-nav").click(function() {
-        $(".content-pane").hide();
-        $(".interventions.content-pane").show();
-        $(".section-bar").find(".active").removeClass("active");
-        $(this).addClass("active");
-    });
-    $(".baseline-nav").click(function() {
-        $(".content-pane").hide();
-        $(".baseline.content-pane").show();
-        $(".section-bar").find(".active").removeClass("active");
-        $(this).addClass("active");
+        $("#interventions").addClass('active show');
     });
 
-    $("[data-toggle='toggle']").click(function() {
+    $("[data-toggle='toggle']").click(function () {
         var selector = $(this).data("target");
         $(selector).addClass('in');
         $(this).closest("ul").find("a").removeClass("active");
         $(this).addClass("active");
+    });
+
+    $("#slider").slider({
+        value: 26,
+        change: function (event, ui) {
+            $("#coverage")
+                .val(ui.value);
+        }
+    });
+
+    $("#coverage").change(function (e) {
+        if (e.target.value !== $('#slider').slider("value"))
+            $('#slider').slider("value", e.target.value)
     });
 });
 
