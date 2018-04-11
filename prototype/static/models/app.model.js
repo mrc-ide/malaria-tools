170af8a1450f2f5c6c1df827576dae8a8ca4f54b
Alex Hill <alex.hill@gmail.com>
2018-04-11 14:25:43 +0100

    new country modal

--- prototype/static/models/app.model.js
+++ prototype/static/models/app.model.js
@@ -3,11 +3,16 @@ var countries = [
 ];
 
 function AppModel() {
+	var self = this;
 	this.countries = ko.observableArray(countries.map(function(x) {
 		return new Country(x, ["Region 1", "Region 2", "Region 3", "Region 4"]);
 	}));
 	this.newScenario = new NewScenario();
-	this.currentCountry = ko.observable(new Country("", []));
+	this.currentCountry = ko.observable(this.countries()[0]);
+	this.selectedCountry = ko.observable(this.countries()[0]);
+	this.newCountry = ko.computed(function(){
+		return new Country(self.selectedCountry().name + "-1",  ["Region 1", "Region 2", "Region 3", "Region 4"])
+	})
 }
 
 function Country(name, regions) {
