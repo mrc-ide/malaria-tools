2b28158e2e54017561e799530f76376dd3f46006
Alex Hill <alex.hill@gmail.com>
2018-04-11 18:58:25 +0100

    edit baseline page

--- prototype/static/models/app.model.js
+++ prototype/static/models/app.model.js
@@ -4,12 +4,16 @@ var countries = [
 
 function AppModel() {
     var self = this;
-    var countryObjects = countries.map(function(x) {
-        return new Country(x, ["Region 1", "Region 2", "Region 3", "Region 4"]);
+    var regionObjects = ["Region 1", "Region 2", "Region 3", "Region 4"].map(function(x){
+        return new Region(x, 2013, 0, 5, {f: ko.observable(20), a: ko.observable(80), g: ko.observable(0)})
+    });
+    var countryObjects = countries.map(function (x) {
+        return new Country(x, regionObjects);
     });
 
     self.mode = ko.observable('scenario');
     self.scenarioMode = ko.observable('intervention');
+    self.regionMode = ko.observable('view');
     self.results = new Results();
 
     self.countries = ko.observableArray(countryObjects);
@@ -20,106 +24,121 @@ function AppModel() {
     self.currentCountry = ko.observable(self.countries()[0]);
     self.currentRegion = ko.observable(self.currentCountry().regions()[0]);
     self.selectedCountry = ko.observable(self.countries()[0]);
-    self.newCountry = ko.computed(function(){
-        return new Country(self.selectedCountry().name + "-1",  ["Region 1", "Region 2", "Region 3", "Region 4"])
+    self.newCountry = ko.computed(function () {
+        return new Country(self.selectedCountry().name + "-1", regionObjects, true)
     });
-    self.addCountry = function(){
+    self.addCountry = function () {
         self.countries.push(self.newCountry());
-        self.countries.sort(function(left, right) {
-            return left.name < right.name ? -1 : 1});
+        self.countries.sort(function (left, right) {
+            return left.name < right.name ? -1 : 1
+        });
 
         self.currentCountry(self.newCountry());
         self.currentRegion(self.newCountry().regions()[0]);
+        self.mode('country');
     };
     self.newScenario = ko.observable(new NewScenario(countryObjects, self));
 
-    self.myLineChart = null;
-    self.myPieChart = null;
-    self.drawLine = function() {
-        if (!self.myLineChart){
-            $.each($('.seasonal'), function() {
-                self.myLineChart = new Chart($(this), {
-                    type: 'line',
-                    data: {
-                        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                        datasets: [{
-                            label: 'rainfall',
-                            borderColor: "#3e95cd",
-                            data: [12, 19, 15, 13, 10, 3, 2, 2, 4, 7, 11, 14]
-                        },
-                            {
-                                label: 'incidence',
-                                borderColor: "#8e5ea2",
-                                data: [11, 12, 18, 15, 13, 9, 4, 3, 3, 5, 8, 12]
-                            }]
+    self.validVectors = ko.computed(function () {
+        var total = parseInt(self.currentRegion().vectors().f())
+            + parseInt(self.currentRegion().vectors().a())
+            + parseInt(self.currentRegion().vectors().g());
+
+        return total === 100;
+    });
+
+    self.drawLine = function () {
+        $.each($('.seasonal'), function () {
+            new Chart($(this), {
+                type: 'line',
+                data: {
+                    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                    datasets: [{
+                        label: 'rainfall',
+                        borderColor: "#3e95cd",
+                        data: [12, 19, 15, 13, 10, 3, 2, 2, 4, 7, 11, 14]
                     },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: "Prevalence"
-                                }
-                            }]
-                        }
+                        {
+                            label: 'incidence',
+                            borderColor: "#8e5ea2",
+                            data: [11, 12, 18, 15, 13, 9, 4, 3, 3, 5, 8, 12]
+                        }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Prevalence"
+                            }
+                        }]
                     }
-                });
-            })
-        }
+                }
+            });
+        })
     };
 
-    self.drawPie = function() {
-        if (!self.myPieChart){
-            $.each($('.vectors'), function() {
-                self.myPieChart = new Chart($(this), {
-                    type: 'pie',
-                    data: {
-                        datasets: [{
-                            data: [20, 0, 80],
-                            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"]
-                        }],
+    self.drawPie = function () {
 
-                        labels: [
-                            'Funestus',
-                            'Arabiensis',
-                            'Gambiae'
-                        ]
-                    },
-                    options: {
-                        title: {
-                            text: "Vectors",
-                            display: true,
-                            position: "left"
-                        }
+        $.each($('.vectors'), function () {
+            new Chart($(this), {
+                type: 'pie',
+                data: {
+                    datasets: [{
+                        data: [20, 0, 80],
+                        backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"]
+                    }],
+
+                    labels: [
+                        'Funestus',
+                        'Arabiensis',
+                        'Gambiae'
+                    ]
+                },
+                options: {
+                    title: {
+                        text: "Vectors",
+                        display: true,
+                        position: "left"
                     }
-                });
-            })
+                }
+            });
+        })
 
-        }
-    }
+    };
 
-    self.showScenarioSection = ko.computed(function() {
+    self.showScenarioSection = ko.computed(function () {
         return self.mode() == "scenario"
-            && self.currentScenario() 
+            && self.currentScenario()
             && self.currentScenario() != "results";
     }, self);
-    self.showResultsSection = ko.computed(function() {
+    self.showResultsSection = ko.computed(function () {
         return self.mode() == "scenario"
             && self.currentScenario() == "results";
     });
-    self.hasResults = ko.computed(function() {
+    self.hasResults = ko.computed(function () {
         //return true;
         return self.scenarios().length > 0;
     }, self);
 
-    self.showResults = function() {
+    self.showResults = function () {
         self.results.render();
         return self.currentScenario("results");
     };
     //self.showResults();
 }
 
-function Country(name, regions) {
+function Country(name, regions, editable) {
     this.name = name;
     this.regions = ko.observableArray(regions);
+    this.editable = editable;
+}
+
+
+function Region(name, year, ageStart, ageEnd, vectors) {
+    this.name = name;
+    this.year = ko.observable(year);
+    this.ageStart = ko.observable(ageStart);
+    this.ageEnd = ko.observable(ageEnd);
+    this.vectors = ko.observable(vectors);
 }
--- prototype/static/script.js
+++ prototype/static/script.js
@@ -1,3 +1,26 @@
+function initSliders() {
+    $(".slider").slider({
+        create: function (event, ui) {
+            $(this).slider('value', $(this).data('initial'));
+        },
+        change: function (event, ui) {
+
+            var selector = $(this).data("control");
+            $(selector)
+                .val(ui.value);
+        }
+    });
+
+    $("[data-role=slider-value]").change(function (e) {
+        var selector = $(this).data("target");
+        var slider = $(selector);
+        if (e.target.value !== slider.slider("value"))
+            slider.slider("value", e.target.value);
+
+    });
+}
+
+
 $(function () {
     ko.applyBindings(new AppModel());
 
@@ -9,23 +32,6 @@ $(function () {
         $(this).toggleClass("active");
     });
 
-    $(".slider").slider({
-        create: function (event, ui) {
-            $(this).slider('value', $(this).data('initial'));
-        },
-        change: function (event, ui) {
-            var selector = $(this).data("control");
-            $(selector)
-                .val(ui.value);
-        }
-    });
-
-    $("[data-role=slider-value]").change(function (e) {
-        var selector = $(this).data("target");
-        var slider = $(selector);
-        if (e.target.value !== slider.slider("value"))
-            slider.slider("value", e.target.value)
-    });
+    initSliders();
 
 });
-
