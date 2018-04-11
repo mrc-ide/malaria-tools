b01e8129047e2fcae5eba064fffdfea19fe7c7be
Martin Eden <martin.s.eden@gmail.com>
2018-04-11 14:27:34 +0100

    All options in new scenario

--- prototype/static/models/app.model.js
+++ prototype/static/models/app.model.js
@@ -3,10 +3,11 @@ var countries = [
 ];
 
 function AppModel() {
-	this.countries = ko.observableArray(countries.map(function(x) {
+	var countryObjects = countries.map(function(x) {
 		return new Country(x, ["Region 1", "Region 2", "Region 3", "Region 4"]);
-	}));
-	this.newScenario = new NewScenario();
+	})
+	this.countries = ko.observableArray(countryObjects);
+	this.newScenario = ko.observable(new NewScenario(countryObjects));
 }
 
 function Country(name, regions) {
@@ -14,7 +15,36 @@ function Country(name, regions) {
 	this.regions = ko.observableArray(regions);
 }
 
-function NewScenario() {
-	this.selectedCountry = ko.observable();
-	this.selectedRegion = ko.observable();
+function NewScenario(countries) {
+	var self = this;
+	self.countries = ko.observableArray(countries);
+	self.selectedCountry = ko.observable();
+	self.selectedRegion = ko.observable();
+	self.name = ko.observable();
+
+	self.selectCountry = function(country, event) {
+		self.selectedCountry(country);
+		self.selectedRegion(null);
+		var item = $(event.target).closest("li");
+		item.closest("ul").find(".collapse").collapse("hide");
+		item.find(".collapse").collapse("show");
+	};
+
+	self.selectRegion = function(region) {
+		self.selectedRegion(region);
+	}
+
+	self.isActive = function(countryOrRegion) {
+		if (self.selectedRegion()) {
+			return self.selectedRegion() == countryOrRegion;
+		} else if (self.selectedCountry()) {
+			return self.selectedCountry() == countryOrRegion;
+		} else {
+			return false;
+		}
+	}
+
+	self.isValid = ko.computed(function() {
+		return this.name() && this.selectedCountry();
+	}, this);
 }
