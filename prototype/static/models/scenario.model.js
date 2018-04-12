db660131c086a7b4bb0b57dfe8ee4920d665741b
Martin Eden <martin.s.eden@gmail.com>
2018-04-12 11:16:28 +0100

    Add checkboxes to scenarios

--- prototype/static/models/scenario.model.js
+++ prototype/static/models/scenario.model.js
@@ -2,4 +2,5 @@ function Scenario(name, country, region) {
 	this.name = ko.observable(name);
 	this.country = ko.observable(country);
 	this.region = ko.observable(region);
+	this.selected = ko.observable(true);
 }
