8a60a0cc76676a19f3fc92c69d8f45c682aa9eb9
Martin Eden <martin.s.eden@pm.me>
2018-04-12 15:20:12 +0100

    Dialog for updating scenarios that use the same baseline.

--- prototype/static/models/scenario.model.js
+++ prototype/static/models/scenario.model.js
@@ -1,6 +1,18 @@
 function Scenario(name, country, region) {
+	var self = this;
 	this.name = ko.observable(name);
 	this.country = ko.observable(country);
 	this.region = ko.observable(region);
 	this.selected = ko.observable(true);
+
+	this.changeCountry = function(newCountry) {
+		self.country(newCountry);
+        var equivalentRegion = null;
+        if (self.region()) {
+            equivalentRegion = newCountry.regions().find(function(r) { 
+                return r.name == self.region().name 
+            });
+        }
+        self.region(equivalentRegion);
+	};
 }
